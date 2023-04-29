41b7d5f04d3803bb464d2e45b38d209a3565960e
haveachin <haveachin@proton.me>
2023-04-29 17:45:38 +0200

    feat: add commit hash link

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,7 +12,10 @@ func main() {
 	log.Println("Starting backend")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello, world!\nCommit hash: " + commitHash))
+
+		link := fmt.Sprintf("<a href=\"https://github.com/mdma-backend/mdma-backend/tree/%s\">%s</a>", commitHash, commitHash)
+
+		w.Write([]byte("<html><body>Hello, world!<br />Commit hash: " + link + "</body></html>"))
 	})
 	log.Println(http.ListenAndServe(":8080", nil))
 }
