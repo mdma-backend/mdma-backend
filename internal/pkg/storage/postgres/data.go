950ac943456795836438a79c59f444c854bbe38f
Daniel Holzwarth <danielholzwarth@gmx.net>
2023-06-01 23:29:52 +0200

    feat: add meshNodes to Aggregated

--- internal/pkg/storage/postgres/data.go
+++ internal/pkg/storage/postgres/data.go
@@ -35,13 +35,14 @@ func (db DB) GetAggregatedData(dataType string, meshNodeUUIDs []string, measured
 	WHERE dt.name = $1
 	`
 
-	query, err := newFunction(measuredStart, query, params, measuredEnd, meshNodeUUIDs)
+	query, params, err := newFunction(measuredStart, query, params, measuredEnd, meshNodeUUIDs)
 	if err != nil {
 		return data.AggregatedData{}, err
 	}
 
 	rows, err := db.pool.Query(query, params...)
 	if err != nil {
+		println("ads")
 		fmt.Println(err)
 		return data.AggregatedData{}, err
 	}
@@ -75,11 +76,11 @@ func (db DB) GetAggregatedData(dataType string, meshNodeUUIDs []string, measured
 	return aggregatedData, nil
 }
 
-func newFunction(measuredStart string, query string, params []interface{}, measuredEnd string, meshNodeUUIDs []string) (string, error) {
+func newFunction(measuredStart string, query string, params []interface{}, measuredEnd string, meshNodeUUIDs []string) (string, []interface{}, error) {
 	if measuredStart != time.Unix(0, 0).String() {
 		startTime, err := time.Parse(time.RFC3339, measuredStart)
 		if err != nil {
-			return "", err
+			return "", nil, err
 		}
 		query += " AND d.measured_at > $" + strconv.Itoa(len(params)+1)
 		params = append(params, startTime)
@@ -88,7 +89,7 @@ func newFunction(measuredStart string, query string, params []interface{}, measu
 	if measuredEnd != time.Unix(0, 0).String() {
 		endTime, err := time.Parse(time.RFC3339, measuredEnd)
 		if err != nil {
-			return "", err
+			return "", nil, err
 		}
 		query += " AND d.measured_at < $" + strconv.Itoa(len(params)+1)
 		params = append(params, endTime)
@@ -102,128 +103,13 @@ func newFunction(measuredStart string, query string, params []interface{}, measu
 			}
 			query += `$` + strconv.Itoa(len(params)+1+i)
 			params = append(params, uuid)
+			println(query)
+			fmt.Println(params)
 		}
 		query += `)`
 	}
-	return query, nil
+	return query, params, nil
 }
-
-/*
-func (db DB) GetAggregatedData(dataType string, meshNodeUUIDs []string, measuredStart string, measuredEnd string, sampleDuration string, sampleCount int, aggregateFunction string) (data.AggregatedData, error) {
-	aggregateFunction = strings.ToLower(aggregateFunction)
-
-	query := `SELECT `
-	if aggregateFunction == "range" {
-		query += `MAX(value) - MIN(value)`
-	} else if aggregateFunction == "count" {
-		query += `COUNT(value)`
-	} else if aggregateFunction == "minimum" {
-		query += `MIN(value)`
-	} else if aggregateFunction == "maximum" {
-		query += `MAX(value)`
-	} else if aggregateFunction == "sum" {
-		query += `SUM(value)`
-	} else if aggregateFunction == "median" {
-		query = `
-			percentile_cont(0.5) WITHIN GROUP (ORDER BY value)
-		`
-	} else if aggregateFunction == "average" {
-		query += `AVG(value)`
-	}
-
-	params := []interface{}{dataType}
-
-	query += `
-	FROM data d
-	JOIN data_type dt ON d.data_type_id = dt.id
-	WHERE dt.name = $1
-	`
-
-	if measuredStart != time.Unix(0, 0).String() {
-		startTime, err := time.Parse("2006-01-02 15:04:05 -0700 MST", measuredStart)
-		if err != nil {
-			return data.AggregatedData{}, err
-		}
-		query += " AND d.measured_at > $2"
-		params = append(params, startTime)
-	}
-
-	if measuredEnd != time.Unix(0, 0).String() {
-		endTime, err := time.Parse("2006-01-02 15:04:05 -0700 MST", measuredEnd)
-		if err != nil {
-			return data.AggregatedData{}, err
-		}
-		query += " AND d.measured_at < $3" + strconv.Itoa(len(meshNodeUUIDs)+3)
-		params = append(params, endTime)
-	}
-
-	if len(meshNodeUUIDs) > 0 {
-		query += `AND controller_id IN (`
-		for i, uuid := range meshNodeUUIDs {
-			if i != 0 {
-				query += `, `
-			}
-			query += `$` + strconv.Itoa(i+4)
-			params = append(params, uuid)
-		}
-		query += `)`
-	}
-
-	if sampleDuration != "" {
-		duration, err := time.ParseDuration(sampleDuration)
-		if err != nil {
-			fmt.Println(err)
-			return data.AggregatedData{}, err
-		}
-
-		sampleRange := duration / time.Duration(sampleCount)
-
-		if aggregateFunction != "median" {
-			query += ` GROUP BY measured_at / $4`
-			params = append(params, sampleRange)
-		}
-	}
-
-	rows, err := db.pool.Query(query, params...)
-	if err != nil {
-		fmt.Println(err)
-		return data.AggregatedData{}, err
-	}
-	defer rows.Close()
-
-	var aggregatedData data.AggregatedData
-	aggregatedData.DataType = dataType
-	aggregatedData.MeshNodeUUIDs = meshNodeUUIDs
-	aggregatedData.AggregateFunction = aggregateFunction
-
-	for rows.Next() {
-		var sampleValue string
-		err := rows.Scan(&sampleValue)
-		if err != nil {
-			fmt.Println(err)
-			return data.AggregatedData{}, err
-		}
-
-		sample := data.Sample{
-			Value: sampleValue,
-		}
-
-		aggregatedData.Samples = append(aggregatedData.Samples, sample)
-	}
-
-	if err := rows.Err(); err != nil {
-		if err == sql.ErrNoRows && aggregateFunction == "median" {
-			fmt.Println(err)
-			return data.AggregatedData{}, errors.New("median not available")
-		}
-		fmt.Println(err)
-		return data.AggregatedData{}, err
-	}
-
-	fmt.Println(err)
-	return aggregatedData, nil
-}
-*/
 
 func (db DB) GetManyData(dataType string, meshNodeUUIDs []string, measuredStart string, measuredEnd string) (data.ManyData, error) {
 	var query = `
