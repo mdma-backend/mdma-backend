10a59c0544584c735edc601e029cd9da1b4f2717
haveachin <haveachin@proton.me>
2023-06-05 03:15:05 +0200

    feat: add high level logic

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.20
 require (
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-chi/cors v1.2.1
+	github.com/go-chi/render v1.0.2
 	github.com/lib/pq v1.10.9
 )
+
+require github.com/ajg/form v1.5.1 // indirect
---
+++ internal/api/role/role.go
@@ -0,0 +1,164 @@
+package role
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"github.com/mdma-backend/mdma-backend/internal/api/role/permission"
+)
+
+type ErrNotFound struct {
+	Err    error
+	RoleID RoleID
+}
+
+func (e ErrNotFound) Error() string {
+	return fmt.Sprintf("role with id %d not found: %s", e.RoleID, e.Err)
+}
+
+type RoleStore interface {
+	RoleByID(RoleID) (Role, error)
+	Roles() ([]Role, error)
+	CreateRole(*Role) error
+	UpdateRole(RoleID, *Role) error
+	DeleteRole(RoleID) error
+}
+
+type RoleID uint
+
+func IDFromString(s string) (RoleID, error) {
+	id, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, fmt.Errorf("%s is not an integer", s)
+	}
+
+	return RoleID(id), nil
+}
+
+type Role struct {
+	ID          RoleID                  `json:"id,omitempty"`
+	CreatedAt   time.Time               `json:"createAt"`
+	UpdatedAt   time.Time               `json:"updatedAt,omitempty"`
+	Name        string                  `json:"name"`
+	Permissions []permission.Permission `json:"permissions"`
+}
+
+type service struct {
+	handler   http.Handler
+	roleStore RoleStore
+}
+
+func NewService(roleStore RoleStore) http.Handler {
+	r := chi.NewRouter()
+	s := service{
+		handler: r,
+	}
+
+	r.Get("/", s.getRoles())
+	r.Get("/{id}", s.getRole())
+	r.Post("/", s.postRole())
+	r.Put("/{id}", s.putRole())
+	r.Delete("/{id}", s.deleteRole())
+
+	return s
+}
+
+func (s service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.handler.ServeHTTP(w, r)
+}
+
+func (s service) getRoles() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		roles, err := s.roleStore.Roles()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		render.JSON(w, r, roles)
+	}
+}
+
+func (s service) getRole() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		roleID, err := IDFromString(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		roles, err := s.roleStore.RoleByID(roleID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		render.JSON(w, r, roles)
+	}
+}
+
+func (s service) postRole() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var role Role
+		if err := json.NewDecoder(r.Body).Decode(&role); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if err := s.roleStore.CreateRole(&role); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		render.Status(r, http.StatusCreated)
+		render.JSON(w, r, role)
+	}
+}
+
+func (s service) putRole() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		roleID, err := IDFromString(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		var role Role
+		if err := json.NewDecoder(r.Body).Decode(&role); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if err := s.roleStore.UpdateRole(roleID, &role); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		render.JSON(w, r, role)
+	}
+}
+
+func (s service) deleteRole() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		roleID, err := IDFromString(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if err := s.roleStore.DeleteRole(roleID); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
