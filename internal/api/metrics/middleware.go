7594d4bc228c239f779f7153f708b17d177d27f0
haveachin <haveachin@proton.me>
2023-06-27 20:08:23 +0200

    feat: add method and anonymize ids

--- internal/api/metrics/middleware.go
+++ internal/api/metrics/middleware.go
@@ -2,7 +2,9 @@ package metrics
 
 import (
 	"net/http"
+	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5/middleware"
@@ -13,16 +15,22 @@ import (
 const (
 	statusCodeLabel = "status_code"
 	endpointLabel   = "endpoint"
+	methodLabel     = "method"
+)
+
+var (
+	uuidPattern = regexp.MustCompile(`.*\/([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})[\/.*]?`)
+	idPattern   = regexp.MustCompile(`.*\/([0-9]+)[\/.*]?`)
 )
 
 var (
 	httpResponseTime = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{Name: "http_response_time"},
-		[]string{statusCodeLabel, endpointLabel},
+		[]string{statusCodeLabel, endpointLabel, methodLabel},
 	)
 	httpCalls = promauto.NewCounterVec(
 		prometheus.CounterOpts{Name: "http_calls"},
-		[]string{statusCodeLabel, endpointLabel},
+		[]string{statusCodeLabel, endpointLabel, methodLabel},
 	)
 )
 
@@ -32,16 +40,30 @@ func Middleware(next http.Handler) http.Handler {
 
 		start := time.Now()
 		defer func() {
+			status := ww.Status()
+			if status == 0 {
+				status = 200
+			}
+
+			path := r.URL.Path
+			if mm := uuidPattern.FindStringSubmatch(path); len(mm) == 2 {
+				path = strings.Replace(path, mm[1], "{uuid}", -1)
+			} else if mm := idPattern.FindStringSubmatch(path); len(mm) == 2 {
+				path = strings.Replace(path, mm[1], "{id}", -1)
+			}
+
 			httpResponseTime.With(
 				prometheus.Labels{
-					statusCodeLabel: strconv.Itoa(ww.Status()),
-					endpointLabel:   r.URL.Path,
+					statusCodeLabel: strconv.Itoa(status),
+					endpointLabel:   path,
+					methodLabel:     r.Method,
 				},
 			).Set(float64(time.Since(start).Seconds()))
 			httpCalls.With(
 				prometheus.Labels{
-					statusCodeLabel: strconv.Itoa(ww.Status()),
-					endpointLabel:   r.URL.Path,
+					statusCodeLabel: strconv.Itoa(status),
+					endpointLabel:   path,
+					methodLabel:     r.Method,
 				},
 			).Inc()
 		}()
